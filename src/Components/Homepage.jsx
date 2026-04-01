504d4360c22889209cf267dcff46519040aa503d
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-04-01 09:49:20 -0700

    changes

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -30,7 +30,7 @@ function Homepage() {
           <div className="hero-image">
             <img
             
-              src="/public/ChatGPT Image Apr 1, 2026, 09_20_08 AM.png"
+              src="/ChatGPT Image Apr 1, 2026, 09_20_08 AM.png"
               alt="Muhammad Saad Mustafa"
             />
           </div>
